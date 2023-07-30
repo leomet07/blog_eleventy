06e53d34f6585c67757641e43b09a288feab15f9
leomet07 <leomet07@gmail.com>
2023-07-30 12:38:42 -0400

    Also display each post's date

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,18 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addFilter("cssmin", function (code) {
 		return new CleanCSS({}).minify(code).styles;
 	});
+
+	// Filters
+	eleventyConfig.addFilter("readableDate", (dateObj) => {
+		// TODO: USE PRETTIER DATE FORMAT
+		return dateObj.toLocaleDateString("en-us", { timeZone: "utc" }) // Use utc tz so that no tz conversion occurs
+	});
+
+	eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+		// Returns YYYY-MM-DD
+		return dateObj.toLocaleDateString("en-CA", {timezone : "utc"}) // Use utc tz so that no tz conversion occurs
+	});
+
 	return {
 		dir: {
 			input: "content", // default: "."
--- content/posts/first.md
+++ content/posts/first.md
@@ -1,5 +1,6 @@
 ---
 title: This is my wonderful first post
+date: 2022-11-01
 ---
 
 ## Secondary Title
