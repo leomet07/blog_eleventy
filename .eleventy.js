a8709d78d7d8442bf07078d6fd5f4cf4774b90ea
leomet07 <leomet07@gmail.com>
2023-07-30 19:28:05 -0400

    Improve date functions to handle null and return more verbose strings

--- .eleventy.js
+++ .eleventy.js
@@ -6,13 +6,27 @@ module.exports = function (eleventyConfig) {
 
 	// Filters
 	eleventyConfig.addFilter("readableDate", (dateObj) => {
+		dateObj ??= new Date(); // if null or undefined, then create new obj with current time
+
+		const dateOptions = {
+			year: "numeric",
+			month: "long",
+			day: "numeric",
+		};
+		
 		// TODO: USE PRETTIER DATE FORMAT
-		return dateObj.toLocaleDateString("en-us", { timeZone: "utc" }) // Use utc tz so that no tz conversion occurs
+		return dateObj.toLocaleDateString("en-us", {
+			timeZone: "utc",
+			...dateOptions,
+		}); // Use utc tz so that no tz conversion occurs
 	});
 
-	eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-		// Returns YYYY-MM-DD
-		return dateObj.toLocaleDateString("en-CA", {timezone : "utc"}) // Use utc tz so that no tz conversion occurs
+	eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+		dateObj ??= new Date(); // if null or undefined, then create new obj with current time
+
+		// Use utc tz so that no tz conversion occurs
+		// Convert to YYYY/MM/DD, then replace forward slashes with dashes to return YYYY-MM-DD
+		return dateObj.toLocaleDateString("en-ZA", { timezone: "utc" }).replace(/\//g, "-");
 	});
 
 	return {
