3f7ba4c78cf6423facbe5e2273cc3f0ad3ce8efc
leomet07 <leomet07@gmail.com>
2023-07-30 20:46:08 -0400

    Set default title and meta description that are overrided by articles

---
+++ _data/metadata.js
@@ -0,0 +1,10 @@
+module.exports = {
+	title: "Lenny's Blog",
+	url: "https://lenny-blog.netlify.app/",
+	language: "en",
+	description: "Active learner with work across the board.",
+	author: {
+		name: "Lenny Metlitsky",
+		url: "https://lennym.dev/",
+	},
+};
